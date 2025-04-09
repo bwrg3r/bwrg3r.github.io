37d62219a0e7c34cf77b6a415b0a50a09fdd4d34
Exberg <yee04005@gmail.com>
2025-04-09 21:50:18 +0800

    Added Web Exploitation to picoCTF2025

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,7 +1,7 @@
 
+import __ASTRO_IMAGE_IMPORT_Z1a1gHu from "./1200x630.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fpicoctf2025%2Findex.mdx";
 import __ASTRO_IMAGE_IMPORT_Z12Tfhr from "../../../public/static/1200x630.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject-a.md";
 import __ASTRO_IMAGE_IMPORT_Z12T7h4 from "../../../public/static/1200x630.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject-b.md";
 import __ASTRO_IMAGE_IMPORT_Z12SYgG from "../../../public/static/1200x630.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject-c.md";
-import __ASTRO_IMAGE_IMPORT_Z1a1gHu from "./1200x630.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fpicoctf2025%2Findex.mdx";
-export default new Map([["../../../public/static/1200x630.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject-a.md", __ASTRO_IMAGE_IMPORT_Z12Tfhr], ["../../../public/static/1200x630.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject-b.md", __ASTRO_IMAGE_IMPORT_Z12T7h4], ["../../../public/static/1200x630.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject-c.md", __ASTRO_IMAGE_IMPORT_Z12SYgG], ["./1200x630.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fpicoctf2025%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z1a1gHu]]);
+export default new Map([["./1200x630.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fpicoctf2025%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z1a1gHu], ["../../../public/static/1200x630.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject-a.md", __ASTRO_IMAGE_IMPORT_Z12Tfhr], ["../../../public/static/1200x630.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject-b.md", __ASTRO_IMAGE_IMPORT_Z12T7h4], ["../../../public/static/1200x630.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject-c.md", __ASTRO_IMAGE_IMPORT_Z12SYgG]]);
 		
