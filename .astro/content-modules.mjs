4a7a7e431fed632681d6838ffee19415bea687d0
Exberg <yee04005@gmail.com>
2025-05-22 09:28:45 +0800

    feat: Added new UMCSCTF finals bakayang solves

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,7 @@
 
 export default new Map([
-["src/content/blog/picoctf2025/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fpicoctf2025%2Findex.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/bbctf2025/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fbbctf2025%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/blog/umcsctf2025-prelim/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fumcsctf2025-prelim%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/blog/umcsctf2025-finals/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fumcsctf2025-finals%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/blog/uvtctf2025/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fuvtctf2025%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
